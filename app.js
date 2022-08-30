38c29af527d260c97adfc86de6658e0e22a0c329
Randyhall91 <randyrocks11@gmail.com>
2022-08-30 15:22:44 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,159 @@
+const packages = [{
+  priorityLevel: 'express',
+  isFragile: false,
+  weight: 2,
+  to: 'Sir Harrington IV',
+  trackingNumber: '1324kjs'
+},
+{
+  priorityLevel: 'standard',
+  isFragile: true,
+  weight: .5,
+  to: 'Master Mercutio',
+  trackingNumber: '1325sdk'
+},
+{
+  priorityLevel: 'free',
+  isFragile: true,
+  weight: .5,
+  to: 'Mistress Ravenfeather',
+  trackingNumber: 'jffd147'
+},
+{
+  priorityLevel: 'standard',
+  isFragile: false,
+  weight: 4,
+  to: 'B. Robert Brown',
+  trackingNumber: 'acdc145'
+},
+{
+  priorityLevel: 'express',
+  isFragile: true,
+  weight: 6,
+  to: 'Chancellor Wallace'
+},
+{
+  priorityLevel: 'standard',
+  isFragile: false,
+  weight: 5,
+  to: 'Sarah Sharm',
+  trackingNumber: '8081baz'
+},
+{
+  priorityLevel: 'free',
+  isFragile: true,
+  weight: 12,
+  to: 'Tae Lien',
+  trackingNumber: 'suz2367'
+}]
+
+
+
+function drawPackages() {
+  let packageitem = document.getElementById('item')
+  let template = ''
+
+  packages.forEach(package => {
+    template += `
+    <div class="row border mt-3 p-3 d-flex flex-row">
+        <div class="col-1">
+          <h1>To:</h1>
+        </div>
+        <div class="col-6">
+          <h1>${package.to}</h1>
+        </div>
+        <div class="col-2"><h1>ID:</h1></div>
+        <div class="col-3"><h1>${package.priorityLevel}</h1></div>
+      </div>
+    `
+  })
+  // @ts-ignore
+  packageitem.innerHTML = template
+}
+function drawHeavyPackages() {
+  let packageitem = document.getElementById('item')
+  let template = ''
+
+  packages.forEach(package => {
+    if (package.weight >= 5) {
+      template += `
+    <div class="row border mt-3 p-3 d-flex flex-row">
+        <div class="col-1">
+          <h1>To:</h1>
+        </div>
+        <div class="col-6">
+          <h1>${package.to}</h1>
+        </div>
+        <div class="col-2"><h1>ID:</h1></div>
+        <div class="col-3"><h1>${package.priorityLevel}</h1></div>
+      </div>
+    `
+    }
+    return
+
+
+  })
+
+  // @ts-ignore
+  packageitem.innerHTML = template
+}
+
+
+function drawPriorityPackages() {
+  let packageitem = document.getElementById('item')
+  let template = ''
+
+  packages.forEach(package => {
+    if (package.priorityLevel == 'express') {
+      template += `
+    <div class="row border mt-3 p-3 d-flex flex-row">
+        <div class="col-1">
+          <h1>To:</h1>
+        </div>
+        <div class="col-6">
+          <h1>${package.to}</h1>
+        </div>
+        <div class="col-2"><h1>ID:</h1></div>
+        <div class="col-3"><h1>${package.priorityLevel}</h1></div>
+      </div>
+    `
+    }
+    return
+
+
+  })
+
+  // @ts-ignore
+  packageitem.innerHTML = template
+}
+function drawFragilePackages() {
+  let packageitem = document.getElementById('item')
+  let template = ''
+
+  packages.forEach(package => {
+    if (package.isFragile == true) {
+      template += `
+    <div class="row border mt-3 p-3 d-flex flex-row">
+        <div class="col-1">
+          <h1>To:</h1>
+        </div>
+        <div class="col-6">
+          <h1>${package.to}</h1>
+        </div>
+        <div class="col-2"><h1>ID:</h1></div>
+        <div class="col-3"><h1>${package.priorityLevel}</h1></div>
+      </div>
+    `
+    }
+    return
+
+
+  })
+
+  // @ts-ignore
+  packageitem.innerHTML = template
+}
+drawPackages()
+
+
+
